cc657d30246b7e6e4983c95621464bba028eb0e6
Tijs Verkoyen <tijs@verkoyen.eu>
2012-07-18 22:56:47 +0200

    Beter code layout

--- tests/index.php
+++ tests/index.php
@@ -7,32 +7,41 @@ require_once '../bpost.php';
 // create instance
 $bpost = new bPost(ACCOUNT_ID, PASSPHRASE);
 
-// $response = $bpost->fetchOrder(660);
+// create notification
+$option =  new bPostNotification('NL', 'tijs@verkoyen.eu');
+
+// create delivery method
+$deliveryMethod = new bPostDeliveryMethodAtHome();
+$deliveryMethod->setNormal(
+	array(
+		'infoDistributed' => $option,
+		'infoNextDay' => $option,
+		'infoReminder' => $option,
+		'automaticSecondPresentation' => '',
+	)
+);
+$deliveryMethod->setInsurance(10);
+
+// create address
+$deliveryAddress = new bPostAddress('Kerkstraat', '108', '9050', 'Gentbrugge');
+
+// create customer
+$customer = new bPostCustomer('Tijs', 'Verkoyen');
+$customer->setDeliveryAddress($deliveryAddress);
+
+// create order
+$order = new bPostOrder(time(), 'OPEN');
+$order->setStatus('OPEN');
+$order->setCostCenter('Vitashop');
+$order->addOrderLine('Item 1', 10);
+$order->addOrderLine('Item 2', 20);
+$order->setCustomer($customer);
+$order->setDeliveryMethod($deliveryMethod);
+$order->setTotal(100);
+
+//$response = $bpost->fetchOrder(660);
 //$response = $bpost->modifyOrderStatus(660, 'OPEN');
 //$response = $bpost->createNationalLabel(660, 8, true, true);
-
-//$deliveryMethod = new bPostDeliveryMethodAtHome();
-//$option =  new bPostNotification('NL', 'tijs@verkoyen.eu');
-//$deliveryMethod->setNormal(
-//	array(
-//		'infoDistributed' => $option,
-//		'infoNextDay' => $option,
-//		'infoReminder' => $option,
-//		'automaticSecondPresentation' => '',
-//	)
-//);
-//$deliveryMethod->setInsurance(10);
-//$deliveryAddress = new bPostAddress('Kerkstraat', '108', '9050', 'Gentbrugge');
-//$customer = new bPostCustomer('Tijs', 'Verkoyen');
-//$customer->setDeliveryAddress($deliveryAddress);
-//$order = new bPostOrder(time(), 'OPEN');
-//$order->setStatus('OPEN');
-//$order->setCostCenter('Vitashop');
-//$order->addOrderLine('Item 1', 10);
-//$order->addOrderLine('Item 2', 20);
-//$order->setCustomer($customer);
-//$order->setDeliveryMethod($deliveryMethod);
-//$order->setTotal(100);
 //$response = $bpost->createOrderAndNationalLabel($order, 5);
 
 // output (Spoon::dump())
