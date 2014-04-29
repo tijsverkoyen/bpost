58dd58dc359943cffc8145ce116fbd104736583e
Tijs Verkoyen <tijs@verkoyen.eu>
2014-04-29 11:32:10 +0200

    Cleanup

--- Bpost.php
+++ Bpost.php
@@ -1,33 +1,13 @@
 <?php
 namespace TijsVerkoyen\Bpost;
 
+use TijsVerkoyen\Bpost\Bpost\Order;
+
 /**
  * Bpost class
  *
- * This source file can be used to communicate with the Bpost Shipping Manager API
- *
- * The class is documented in the file itself. If you find any bugs help me out and report them. Reporting can be done by sending an email to php-bpost-bugs[at]verkoyen[dot]eu.
- * If you report a bug, make sure you give me enough information (include your code).
- *
- * Changelog since 1.0.1
- * - better errorhandling
- *
- * Changelog since 1.0.0
- * - added a class to handle the form.
- *
- * License
- * Copyright (c), Tijs Verkoyen. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products derived from this software without specific prior written permission.
- *
- * This software is provided by the author "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the author be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
- *
  * @author    Tijs Verkoyen <php-bpost@verkoyen.eu>
- * @version   1.0.1
+ * @version   3.0.0
  * @copyright Copyright (c), Tijs Verkoyen. All rights reserved.
  * @license   BSD License
  */
