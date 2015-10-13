59be4a199be9153aa9c7f3a1ad5912b182693e9f
kouinkouin <me@kouinkouin.com>
2015-10-13 15:47:47 +0200

    Create Bpost\ProductConfiguration\Option class

---
+++ Bpost/ProductConfiguration/Option.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace TijsVerkoyen\Bpost\Bpost\ProductConfiguration;
+
+use SimpleXMLElement;
+
+class Option
+{
+
+    /** @var  string */
+    private $visibility;
+    /** @var  int */
+    private $price;
+    /** @var  string */
+    private $name;
+    /** @var Characteristic[] */
+    private $characteristics = array();
+
+    /**
+     * @param SimpleXMLElement $xml
+     *
+     * @return Option
+     */
+    public static function createFromXML(SimpleXMLElement $xml)
+    {
+        /*
+        <option visiblity="NOT_VISIBLE_BY_CONSUMER_OPTIONAL" price="0" name="Cash on delivery"/>
+        */
+        $attributes = $xml->attributes();
+        $children = $xml->children();
+
+        $option = new self();
+        $option->setVisibility($attributes['visibility']);
+        $option->setPrice($attributes['price']);
+        $option->setName($attributes['name']);
+
+        if (isset($children->chracteristic)) {
+            foreach ($children->chracteristic as $characteristicXml) {
+                $option->addCharacteristic(Characteristic::createFromXML($characteristicXml));
+            }
+        }
+
+        return $option;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVisibility()
+    {
+        return $this->visibility;
+    }
+
+    /**
+     * @param string $visibility
+     */
+    public function setVisibility($visibility)
+    {
+        $this->visibility = (string) $visibility;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param int $price
+     */
+    public function setPrice($price)
+    {
+        $this->price = (int) $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = (string) $name;
+    }
+
+    /**
+     * @return Characteristic[]
+     */
+    public function getCharacteristics()
+    {
+        return $this->characteristics;
+    }
+
+    /**
+     * @param Characteristic $characteristic
+     */
+    public function addCharacteristic(Characteristic $characteristic)
+    {
+        $this->characteristics[] = $characteristic;
+    }
+
+}
