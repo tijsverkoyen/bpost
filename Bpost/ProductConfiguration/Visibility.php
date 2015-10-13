34a2aaadd94a893e5ccfae2cc7e45744db2f6a1c
kouinkouin <me@kouinkouin.com>
2015-10-13 15:48:08 +0200

    Create Bpost\ProductConfiguration\Visibility class

---
+++ Bpost/ProductConfiguration/Visibility.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace TijsVerkoyen\Bpost\Bpost\ProductConfiguration;
+
+class Visibility
+{
+    const VISIBILITY_VISIBLE = 'VISIBLE';
+    const VISIBILITY_NOT_VISIBLE_BY_CONSUMER_OPTIONAL = 'NOT_VISIBLE_BY_CONSUMER_OPTIONAL';
+}
