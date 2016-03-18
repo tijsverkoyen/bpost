2a5310087b6ad35d1c5538a3ca16f4ae50ef1434
Raphaël Pommier <raphael.pommier@antidot.com>
2016-03-18 20:23:53 +0100

    feat (ValidatedValue): Add class

---
+++ src/Common/ValidatedValue.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace TijsVerkoyen\Bpost\Common;
+
+use TijsVerkoyen\Bpost\Exception\BpostLogicException;
+use TijsVerkoyen\Bpost\Exception\BpostLogicException\BpostInvalidLengthException;
+use TijsVerkoyen\Bpost\Exception\BpostLogicException\BpostInvalidPatternException;
+use TijsVerkoyen\Bpost\Exception\BpostLogicException\BpostInvalidValueException;
+
+/**
+ * Class ValidatedValue
+ */
+abstract class ValidatedValue
+{
+    /** @var mixed */
+    private $value;
+
+    /**
+     * ValidatedValue constructor.
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        $this->setValue($value);
+        $this->validate();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string)$this->getValue();
+    }
+
+    /**
+     * @param int $length
+     * @throws BpostInvalidLengthException
+     */
+    public function validateLength($length)
+    {
+        if (mb_strlen($this->getValue()) > $length) {
+            throw new BpostInvalidLengthException('', mb_strlen($this->getValue()), $length);
+        }
+    }
+
+    /**
+     * @param array $allowedValues
+     * @throws BpostInvalidValueException
+     */
+    public function validateChoice(array $allowedValues)
+    {
+        if (!in_array($this->getValue(), $allowedValues)) {
+            throw new BpostInvalidValueException('', $this->getValue(), $allowedValues);
+        }
+    }
+
+    /**
+     * @param string $regexPattern
+     * @throws BpostInvalidPatternException
+     */
+    public function validatePattern($regexPattern)
+    {
+        if (!preg_match("/^$regexPattern\$/", $this->getValue())) {
+            throw new BpostInvalidPatternException('', $this->getValue(), $regexPattern);
+        }
+    }
+
+    /**
+     * @throws BpostLogicException
+     */
+    public abstract function validate();
+
+}
---
+++ tests/Common/ValidatedValueTest.php
@@ -0,0 +1,95 @@
+<?php
+
+use TijsVerkoyen\Bpost\Common\ValidatedValue;
+use TijsVerkoyen\Bpost\Exception\BpostLogicException;
+use TijsVerkoyen\Bpost\Exception\BpostLogicException\BpostInvalidLengthException;
+use TijsVerkoyen\Bpost\Exception\BpostLogicException\BpostInvalidPatternException;
+use TijsVerkoyen\Bpost\Exception\BpostLogicException\BpostInvalidValueException;
+
+class ValidatedValueFake extends ValidatedValue
+{
+
+    /**
+     * @throws BpostLogicException
+     */
+    public function validate()
+    {
+        if ($this->getValue() === 'aze') {
+            throw new BpostLogicException('aze');
+        }
+    }
+}
+
+class ValidatedValueTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetValue()
+    {
+        $fake = new ValidatedValueFake('qsd');
+        $this->assertSame('qsd', $fake->getValue());
+
+        $fake = new ValidatedValueFake('qsd');
+        $this->assertSame('qsd', (string)$fake);
+
+        $this->setExpectedException('TijsVerkoyen\Bpost\Exception\BpostLogicException');
+        new ValidatedValueFake('aze');
+    }
+
+    public function testValidateLength()
+    {
+        $fake = new ValidatedValueFake('qsd');
+        try {
+            $fake->validateLength(10);
+            $this->assertTrue(true);
+        } catch (BpostInvalidLengthException $e) {
+            $this->fail('Exception launched for valid value: "qsd" (tested with length=10)');
+        }
+
+        try {
+            $fake->validateLength(2);
+            $this->fail('Exception uncaught for invalid value: "qsd" (tested with length=2)');
+        } catch (BpostInvalidLengthException $e) {
+            $this->assertTrue(true);
+        }
+
+    }
+
+    public function testValidateChoice()
+    {
+        $fake = new ValidatedValueFake('qsd');
+        try {
+            $fake->validateChoice(array('aze', 'qsd'));
+            $this->assertTrue(true);
+        } catch (BpostInvalidValueException $e) {
+            $this->fail('Exception launched for valid value: "qsd" (tested with ["aze", "qsd"])');
+        }
+
+        try {
+            $fake->validateChoice(array('aze', 'wxc'));
+            $this->fail('Exception uncaught for invalid value: "qsd" (tested with ["aze", "wxc"])');
+        } catch (BpostInvalidValueException $e) {
+            $this->assertTrue(true);
+        }
+
+    }
+
+    public function testValidatePattern()
+    {
+        $fake = new ValidatedValueFake('mail2016@antidot.com');
+        try {
+            $fake->validatePattern('([a-zA-Z0-9_\.\-+])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+');
+            $this->assertTrue(true);
+        } catch (BpostInvalidPatternException $e) {
+            $this->fail('Exception launched for valid value: "mail2016@antidot.com"');
+        }
+
+        try {
+            $fake->validatePattern('([a-zA-Z0-9_\.\-+])+(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+');
+            $this->fail('Exception uncaught for invalid value: "mail2016@antidot.com"');
+        } catch (BpostInvalidPatternException $e) {
+            $this->assertTrue(true);
+        }
+
+    }
+
+}
