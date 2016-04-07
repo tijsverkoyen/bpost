fb2053a563a582d03d96f2489f5971323d610ada
Raphaël Pommier <raphael.pommier@antidot.com>
2016-04-07 12:11:57 +0200

    test (Geo6): Refactor the test

--- tests/Geo6/PoiTest.php
+++ tests/Geo6/PoiTest.php
@@ -11,40 +11,108 @@ class PoiTest extends \PHPUnit_Framework_TestCase
      */
     public function testCreateFromXml()
     {
-        $data = array(
-            'Id' => '405700',
-            'Type' => '1',
-            'Name' => 'ST-AMANDSBERG HOGEWEG',
-            'Street' => 'Hogeweg',
-            'Number' => '108',
-            'Zip' => '9040',
-            'City' => 'Sint-Amandsberg',
-            'X' => 106482,
-            'Y' => 195899,
-            'Longitude' => 3.7479,
-            'Latitude' => 51.0716,
-        );
-
-        // build xml
-        $xmlString = '<record>';
-        foreach ($data as $key => $value) {
-            $xmlString .= '<' . $key . '>' . $value . '</' . $key . '>';
-        }
-        $xmlString .= '</record>';
-        $xml = simplexml_load_string($xmlString);
+        $xml = simplexml_load_string($this->getXml());
 
         $poi = Poi::createFromXML($xml);
 
-        $this->assertSame($data['Id'], $poi->getId());
-        $this->assertSame($data['Type'], $poi->getType());
-        $this->assertSame($data['Name'], $poi->getOffice());
-        $this->assertSame($data['Street'], $poi->getStreet());
-        $this->assertSame($data['Number'], $poi->getNr());
-        $this->assertSame($data['Zip'], $poi->getZip());
-        $this->assertSame($data['City'], $poi->getCity());
-        $this->assertSame($data['X'], $poi->getX());
-        $this->assertSame($data['Y'], $poi->getY());
-        $this->assertSame($data['Longitude'], $poi->getLongitude());
-        $this->assertSame($data['Latitude'], $poi->getLatitude());
+        $this->assertSame('220000', $poi->getId());
+        $this->assertSame('1', $poi->getType());
+        $this->assertSame('GENT CENTRUM', $poi->getOffice());
+        $this->assertSame('Lange Kruisstraat', $poi->getStreet());
+        $this->assertSame('55', $poi->getNr());
+        $this->assertSame('9000', $poi->getZip());
+        $this->assertSame('Gent', $poi->getCity());
+        $this->assertSame(104918, $poi->getX());
+        $this->assertSame(193708, $poi->getY());
+        $this->assertSame(3.72581, $poi->getLongitude());
+        $this->assertSame(51.05178, $poi->getLatitude());
+        $this->assertNotNull($poi->getHours());
+
+        $hours = $poi->getHours();
+        $this->assertCount(7, $hours);
+        $this->assertArrayHasKey(1, $hours); // Monday
+
+        $this->assertSame('9:00', $hours[1]->getAmOpen());
+        $this->assertNull($hours[1]->getAmClose());
+        $this->assertNull($hours[1]->getPmOpen());
+        $this->assertSame('18:00', $hours[1]->getPmClose());
+
+        $this->assertNull($poi->getClosedFrom());
+        $this->assertNull($poi->getClosedTo());
+        $this->assertNull($poi->getNote());
+
+    }
+
+    /**
+     * @return string
+     */
+    private function getXml()
+    {
+        return <<< XML
+<Record>
+  <ID>220000</ID>
+  <Type>1</Type>
+  <OFFICE>GENT CENTRUM</OFFICE>
+  <STREET>Lange Kruisstraat</STREET>
+  <NR>55</NR>
+  <ZIP>9000</ZIP>
+  <CITY>Gent</CITY>
+  <X>104918</X>
+  <Y>193708</Y>
+  <Longitude>3.72581</Longitude>
+  <Latitude>51.05178</Latitude>
+  <Services>
+    <Service category="2" flag="10">Loket met Bancontact/Mistercash</Service>
+  </Services>
+  <Hours>
+    <Monday>
+      <AMOpen>9:00</AMOpen>
+      <AMClose></AMClose>
+      <PMOpen></PMOpen>
+      <PMClose>18:00</PMClose>
+    </Monday>
+    <Tuesday>
+      <AMOpen>9:00</AMOpen>
+      <AMClose></AMClose>
+      <PMOpen></PMOpen>
+      <PMClose>18:00</PMClose>
+    </Tuesday>
+    <Wednesday>
+      <AMOpen>9:00</AMOpen>
+      <AMClose></AMClose>
+      <PMOpen></PMOpen>
+      <PMClose>18:00</PMClose>
+    </Wednesday>
+    <Thursday>
+      <AMOpen>9:00</AMOpen>
+      <AMClose></AMClose>
+      <PMOpen></PMOpen>
+      <PMClose>18:00</PMClose>
+    </Thursday>
+    <Friday>
+      <AMOpen>9:00</AMOpen>
+      <AMClose></AMClose>
+      <PMOpen></PMOpen>
+      <PMClose>18:00</PMClose>
+    </Friday>
+    <Saturday>
+      <AMOpen>9:00</AMOpen>
+      <AMClose></AMClose>
+      <PMOpen></PMOpen>
+      <PMClose>15:00</PMClose>
+    </Saturday>
+    <Sunday>
+      <AMOpen></AMOpen>
+      <AMClose></AMClose>
+      <PMOpen></PMOpen>
+      <PMClose></PMClose>
+    </Sunday>
+  </Hours>
+  <ClosedFrom></ClosedFrom>
+  <ClosedTo></ClosedTo>
+  <NOTE></NOTE>
+</Record>
+XML;
+
     }
 }
