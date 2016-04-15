85128a5004beee79a99f6f523c7699ceb20a3b04
Gaël Lebrun <glebrun@antidot.com>
2016-04-15 10:05:10 +0200

    test (BpostNotImplementedException): Add test for BpostNotImplementedException

---
+++ tests/Exception/BpostNotImplementedExceptionTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bpost\BpostApiClient\test\Exception;
+
+use Bpost\BpostApiClient\Exception\BpostNotImplementedException;
+
+class BpostNotImplementedExceptionTest extends \PHPUnit_Framework_TestCase
+
+{
+    public function testGetMessage()
+    {
+        $ex = new BpostNotImplementedException();
+        $this->assertContains('Not implemented', $ex->getMessage());
+    }
+}
