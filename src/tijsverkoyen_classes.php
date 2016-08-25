e83691189cc116c45506434f910c37e5c1ff905a
Raphaël Pommier <raphael.pommier@antidot.com>
2016-08-25 20:24:11 +0200

    refactor: declare old TijsVerkoyen\bpost classes as deprecated

--- src/tijsverkoyen_classes.php
+++ src/tijsverkoyen_classes.php
@@ -1,36 +1,35 @@
 <?php
-namespace TijsVerkoyen\Bpost { class Bpack247 extends \Bpost\BpostApiLibrary\Bpack247 {} } // src/Bpack247.php
-namespace TijsVerkoyen\Bpost\Bpack247 { class Customer extends \Bpost\BpostApiLibrary\Bpack247\Customer {} } // src/Bpack247/Customer.php
-namespace TijsVerkoyen\Bpost\Bpack247 { class CustomerPackStation extends \Bpost\BpostApiLibrary\Bpack247\CustomerPackStation {} } // src/Bpack247/CustomerPackStation.php
-namespace TijsVerkoyen\Bpost { class Bpost extends \Bpost\BpostApiLibrary\Bpost {} } // src/Bpost.php
-namespace TijsVerkoyen\Bpost\Bpost { class Label extends \Bpost\BpostApiLibrary\Bpost\Label {} } // src/Bpost/Label.php
-namespace TijsVerkoyen\Bpost\Bpost { class Order extends \Bpost\BpostApiLibrary\Bpost\Order {} } // src/Bpost/Order.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class Address extends \Bpost\BpostApiLibrary\Bpost\Order\Address {} } // src/Bpost/Order/Address.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class Box extends \Bpost\BpostApiLibrary\Bpost\Order\Box {} } // src/Bpost/Order/Box.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box { class At247 extends \Bpost\BpostApiLibrary\Bpost\Order\Box\At247 {} } // src/Bpost/Order/Box/At247.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box { class AtBpost extends \Bpost\BpostApiLibrary\Bpost\Order\Box\AtBpost {} } // src/Bpost/Order/Box/AtBpost.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box { class AtHome extends \Bpost\BpostApiLibrary\Bpost\Order\Box\AtHome {} } // src/Bpost/Order/Box/AtHome.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Customsinfo { class CustomsInfo extends \Bpost\BpostApiLibrary\Bpost\Order\Box\CustomsInfo\CustomsInfo {} } // src/Bpost/Order/Box/Customsinfo/CustomsInfo.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box { interface IBox extends \Bpost\BpostApiLibrary\Bpost\Order\Box\IBox {} } // src/Bpost/Order/Box/IBox.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box { class International extends \Bpost\BpostApiLibrary\Bpost\Order\Box\International {} } // src/Bpost/Order/Box/International.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box { abstract class National extends \Bpost\BpostApiLibrary\Bpost\Order\Box\National {} } // src/Bpost/Order/Box/National.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Openinghour { class Day extends \Bpost\BpostApiLibrary\Bpost\Order\Box\OpeningHour\Day {} } // src/Bpost/Order/Box/OpeningHour/Day.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { class AutomaticSecondPresentation extends \Bpost\BpostApiLibrary\Bpost\Order\Box\Option\AutomaticSecondPresentation {} } // src/Bpost/Order/Box/Option/AutomaticSecondPresentation.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { class CashOnDelivery extends \Bpost\BpostApiLibrary\Bpost\Order\Box\Option\CashOnDelivery {} } // src/Bpost/Order/Box/Option/CashOnDelivery.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { class Insurance extends \Bpost\BpostApiLibrary\Bpost\Order\Box\Option\Insurance {} } // src/Bpost/Order/Box/Option/Insurance.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { class Messaging extends \Bpost\BpostApiLibrary\Bpost\Order\Box\Option\Messaging {} } // src/Bpost/Order/Box/Option/Messaging.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { class Option extends \Bpost\BpostApiLibrary\Bpost\Order\Box\Option\Option {} } // src/Bpost/Order/Box/Option/Option.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { class SaturdayDelivery extends \Bpost\BpostApiLibrary\Bpost\Order\Box\Option\SaturdayDelivery {} } // src/Bpost/Order/Box/Option/SaturdayDelivery.php
-namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { class Signature extends \Bpost\BpostApiLibrary\Bpost\Order\Box\Option\Signature {} } // src/Bpost/Order/Box/Option/Signature.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class Customer extends \Bpost\BpostApiLibrary\Bpost\Order\Customer {} } // src/Bpost/Order/Customer.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class Line extends \Bpost\BpostApiLibrary\Bpost\Order\Line {} } // src/Bpost/Order/Line.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class ParcelsDepotAddress extends \Bpost\BpostApiLibrary\Bpost\Order\ParcelsDepotAddress {} } // src/Bpost/Order/ParcelsDepotAddress.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class PugoAddress extends \Bpost\BpostApiLibrary\Bpost\Order\PugoAddress {} } // src/Bpost/Order/PugoAddress.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class Receiver extends \Bpost\BpostApiLibrary\Bpost\Order\Receiver {} } // src/Bpost/Order/Receiver.php
-namespace TijsVerkoyen\Bpost\Bpost\Order { class Sender extends \Bpost\BpostApiLibrary\Bpost\Order\Sender {} } // src/Bpost/Order/Sender.php
-namespace TijsVerkoyen\Bpost { class Exception extends \Bpost\BpostApiLibrary\Exception {} } // src/Exception.php
-namespace TijsVerkoyen\Bpost { class FormHandler extends \Bpost\BpostApiLibrary\FormHandler {} } // src/FormHandler.php
-namespace TijsVerkoyen\Bpost { class Geo6 extends \Bpost\BpostApiLibrary\Geo6 {} } // src/Geo6.php
-namespace TijsVerkoyen\Bpost\Geo6 { class Day extends \Bpost\BpostApiLibrary\Geo6\Day {} } // src/Geo6/Day.php
-namespace TijsVerkoyen\Bpost\Geo6 { class Poi extends \Bpost\BpostApiLibrary\Geo6\Poi {} } // src/Geo6/Poi.php
-namespace TijsVerkoyen\Bpost\Geo6 { class Service extends \Bpost\BpostApiLibrary\Geo6\Service {} } // src/Geo6/Service.php
+namespace TijsVerkoyen\Bpost { /** @deprecated */ class Bpack247 extends \Bpost\BpostApiClient\Bpack247 {} } // src/Bpack247.php
+namespace TijsVerkoyen\Bpost\Bpack247 { /** @deprecated */ class Customer extends \Bpost\BpostApiClient\Bpack247\Customer {} } // src/Bpack247/Customer.php
+namespace TijsVerkoyen\Bpost\Bpack247 { /** @deprecated */ class CustomerPackStation extends \Bpost\BpostApiClient\Bpack247\CustomerPackStation {} } // src/Bpack247/CustomerPackStation.php
+namespace TijsVerkoyen\Bpost { /** @deprecated */ class Bpost extends \Bpost\BpostApiClient\Bpost {} } // src/Bpost.php
+namespace TijsVerkoyen\Bpost\Bpost { /** @deprecated */ class Label extends \Bpost\BpostApiClient\Bpost\Label {} } // src/Bpost/Label.php
+namespace TijsVerkoyen\Bpost\Bpost { /** @deprecated */ class Order extends \Bpost\BpostApiClient\Bpost\Order {} } // src/Bpost/Order.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class Address extends \Bpost\BpostApiClient\Bpost\Order\Address {} } // src/Bpost/Order/Address.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class Box extends \Bpost\BpostApiClient\Bpost\Order\Box {} } // src/Bpost/Order/Box.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box { /** @deprecated */ class At247 extends \Bpost\BpostApiClient\Bpost\Order\Box\At247 {} } // src/Bpost/Order/Box/At247.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box { /** @deprecated */ class AtBpost extends \Bpost\BpostApiClient\Bpost\Order\Box\AtBpost {} } // src/Bpost/Order/Box/AtBpost.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box { /** @deprecated */ class AtHome extends \Bpost\BpostApiClient\Bpost\Order\Box\AtHome {} } // src/Bpost/Order/Box/AtHome.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Customsinfo { /** @deprecated */ class CustomsInfo extends \Bpost\BpostApiClient\Bpost\Order\Box\CustomsInfo\CustomsInfo {} } // src/Bpost/Order/Box/Customsinfo/CustomsInfo.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box { interface IBox extends \Bpost\BpostApiClient\Bpost\Order\Box\IBox {} } // src/Bpost/Order/Box/IBox.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box { /** @deprecated */ class International extends \Bpost\BpostApiClient\Bpost\Order\Box\International {} } // src/Bpost/Order/Box/International.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box { abstract class National extends \Bpost\BpostApiClient\Bpost\Order\Box\National {} } // src/Bpost/Order/Box/National.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Openinghour { /** @deprecated */ class Day extends \Bpost\BpostApiClient\Bpost\Order\Box\OpeningHour\Day {} } // src/Bpost/Order/Box/OpeningHour/Day.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { /** @deprecated */ class AutomaticSecondPresentation extends \Bpost\BpostApiClient\Bpost\Order\Box\Option\AutomaticSecondPresentation {} } // src/Bpost/Order/Box/Option/AutomaticSecondPresentation.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { /** @deprecated */ class CashOnDelivery extends \Bpost\BpostApiClient\Bpost\Order\Box\Option\CashOnDelivery {} } // src/Bpost/Order/Box/Option/CashOnDelivery.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { /** @deprecated */ class Insurance extends \Bpost\BpostApiClient\Bpost\Order\Box\Option\Insurance {} } // src/Bpost/Order/Box/Option/Insurance.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { /** @deprecated */ class Messaging extends \Bpost\BpostApiClient\Bpost\Order\Box\Option\Messaging {} } // src/Bpost/Order/Box/Option/Messaging.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { /** @deprecated */ class SaturdayDelivery extends \Bpost\BpostApiClient\Bpost\Order\Box\Option\SaturdayDelivery {} } // src/Bpost/Order/Box/Option/SaturdayDelivery.php
+namespace TijsVerkoyen\Bpost\Bpost\Order\Box\Option { /** @deprecated */ class Signature extends \Bpost\BpostApiClient\Bpost\Order\Box\Option\Signature {} } // src/Bpost/Order/Box/Option/Signature.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class Customer extends \Bpost\BpostApiClient\Bpost\Order\Customer {} } // src/Bpost/Order/Customer.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class Line extends \Bpost\BpostApiClient\Bpost\Order\Line {} } // src/Bpost/Order/Line.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class ParcelsDepotAddress extends \Bpost\BpostApiClient\Bpost\Order\ParcelsDepotAddress {} } // src/Bpost/Order/ParcelsDepotAddress.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class PugoAddress extends \Bpost\BpostApiClient\Bpost\Order\PugoAddress {} } // src/Bpost/Order/PugoAddress.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class Receiver extends \Bpost\BpostApiClient\Bpost\Order\Receiver {} } // src/Bpost/Order/Receiver.php
+namespace TijsVerkoyen\Bpost\Bpost\Order { /** @deprecated */ class Sender extends \Bpost\BpostApiClient\Bpost\Order\Sender {} } // src/Bpost/Order/Sender.php
+namespace TijsVerkoyen\Bpost { /** @deprecated */ class Exception extends \Bpost\BpostApiClient\Exception {} } // src/Exception.php
+namespace TijsVerkoyen\Bpost { /** @deprecated */ class FormHandler extends \Bpost\BpostApiClient\FormHandler {} } // src/FormHandler.php
+namespace TijsVerkoyen\Bpost { /** @deprecated */ class Geo6 extends \Bpost\BpostApiClient\Geo6 {} } // src/Geo6.php
+namespace TijsVerkoyen\Bpost\Geo6 { /** @deprecated */ class Day extends \Bpost\BpostApiClient\Geo6\Day {} } // src/Geo6/Day.php
+namespace TijsVerkoyen\Bpost\Geo6 { /** @deprecated */ class Poi extends \Bpost\BpostApiClient\Geo6\Poi {} } // src/Geo6/Poi.php
+namespace TijsVerkoyen\Bpost\Geo6 { /** @deprecated */ class Service extends \Bpost\BpostApiClient\Geo6\Service {} } // src/Geo6/Service.php
